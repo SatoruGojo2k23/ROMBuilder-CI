1b1e8d68a90dc21d00eddc33d75e7262ac46c423
Gojo Satoru <133470935+SatoruGojo2k23@users.noreply.github.com>
2023-07-01 14:31:34 +0600

    Update patches for crdroid-13.0
    Signed-off-by: Gojo Satoru <133470935+SatoruGojo2k23@users.noreply.github.com>

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -29,4 +29,23 @@ git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_ho
 #Clone Ims
 git clone --depth=1 https://github.com/ArrowOS-Devices/android_vendor_realme_RMX2020-ims vendor/realme/RMX2020-ims;
 
+#fw/native patch
+cd frameworks/native || exit 1
+curl https://github.com/begonia-dev/android_frameworks_native/commit/629aa1b40ceb83b57c8bd4803036013029586e11.patch | git am -3
+cd ../..
+
+#adding patch fw/base
+rm -rf frameworks/base && git clone --depth=1 https://github.com/SatoruGojo2k23/CrDroid-android_frameworks_base frameworks/base;
+
+#adding patch fw/av
+rm -rf frameworks/av && git clone --depth=1 https://github.com/SatoruGojo2k23/CrDroid-android_frameworks_av frameworks/av;
+
+#adding patch packages/apps
+cd packages/apps/Settings || exit 1
+curl https://github.com/begonia-dev/android_packages_apps_Settings/commit/a88066cb1451edb42e75eb66d3b6b8a3dbcba9a6.patch | git am -3 
+cd ../../..
+
+#adding patch wifi
+cd packages/modules/Wifi && git fetch https://github.com/AOSP-13-RMX2020/packages_modules_Wifi && git cherry-pick 1315ccb757bd2d7c63b4815ab77e04535d2b7750^..6b341eefeb1127a97dc3b77a853e30ed7630be30 && cd ../../..
+
 exit 0;
