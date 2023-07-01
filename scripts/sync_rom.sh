7e769c4cc206657cd16b1bb4acf263ac256d0f8c
Gojo Satoru <133470935+SatoruGojo2k23@users.noreply.github.com>
2023-07-01 07:38:32 +0600

    Update sync_rom.sh
    Signed-off-by: Gojo Satoru <133470935+SatoruGojo2k23@users.noreply.github.com>

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -17,13 +17,10 @@ git clone $DEVICE_REPO --depth=1 -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
 git clone $VENDOR_REPO --depth=1 -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
 git clone $KERNEL_REPO --depth=1 -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
 
-# Clone Realme stuffs
-git clone --depth=1 https://github.com/Realme-G70-Series/android_packages_apps_RealmeParts -b lineage-18.1 packages/apps/RealmeParts && git clone --depth=1 https://github.com/Realme-G70-Series/android_packages_apps_RealmeDirac -b lineage-18.1 packages/apps/RealmeDirac;
-
-# Selinux
-cd external/selinux && curl -L http://ix.io/2FhM > sasta.patch && git am sasta.patch && cd ../..;
-
-# Audio
-cd frameworks/av && wget https://github.com/phhusson/platform_frameworks_av/commit/624cfc90b8bedb024f289772960f3cd7072fa940.patch && patch -p1 < *.patch && cd -;
+#clone more
+git clone --depth=1 https://github.com/ArrowOS/android_hardware_mediatek hardware/mediatek;
+git clone --depth=1 https://github.com/ArrowOS/android_device_mediatek_sepolicy_vndr -b arrow-13.1 device/mediatek/sepolicy_vndr;
+git clone --depth=1 https://github.com/sarthakroy2002/android_prebuilts_clang_host_linux-x86_clang-r437112 prebuilts/clang/host/linux-x86/clang-r437112;
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_vendor_realme_RMX2020-ims vendor/realme/RMX2020-ims;
 
 exit 0;
